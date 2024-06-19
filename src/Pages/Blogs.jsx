6a9123e24338633c9c6638d90c6185ac81829d97
tasib94 <rahi84974@gmail.com>
2024-06-19 08:46:48 -0600

    update cover image and date

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -9,10 +9,12 @@ const Blogs = () => {
         <section className="">
             <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
                 <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 ">
-                    <img src="https://i.postimg.cc/ydGMwx3B/download-25.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 " />
+                    <img src={blogs[0].cover_image} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 " />
                     <div className="p-6 space-y-2 lg:col-span-5">
                         <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
-                        <span className="text-xs text-gray-400">February 19, 2021</span>
+                        <span className="text-xs text-gray-400">
+                            {new Date(blogs[0].published_at).toLocaleDateString()}
+                        </span>
                         <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
                     </div>
                 </a>
